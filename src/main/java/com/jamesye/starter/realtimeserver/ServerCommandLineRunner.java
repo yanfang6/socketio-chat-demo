df28fff2cd7b2c3433e68f97cc7253c3ccf5b091
yanfang <79782136@qq.com>
2019-05-21 11:12:57 +0800

    first commit

---
+++ src/main/java/com/jamesye/starter/realtimeserver/ServerCommandLineRunner.java
@@ -0,0 +1,23 @@
+package com.jamesye.starter.realtimeserver;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import io.netty.util.concurrent.OrderedEventExecutor;
+
+@Component
+public class ServerCommandLineRunner implements CommandLineRunner {
+
+    private final SocketIOServer server;
+
+    @Autowired
+    public ServerCommandLineRunner(SocketIOServer server) {
+        this.server = server;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        server.start();
+    }
+}
